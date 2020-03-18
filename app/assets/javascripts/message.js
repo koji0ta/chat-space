de2f07b18896f451d3b550cc73bf421ff0b0498f
koji0ta <tentman.programingman@gmail.com>
2020-03-18 12:38:28 +0900

    JSの記述も訂正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,7 +6,7 @@ $(function() {
       var html = `<div class="message" data-message-id= ${message.id}> 
                     <div class="upper-message">
                       <div class="upper-message__user-name">
-                        message.user_name
+                        ${message.user_name}
                       </div>
                       <div class="upper-message__date">
                         ${message.created_at}
